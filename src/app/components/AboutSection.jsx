cc43639c86d00e0af4a206e8cf395258b9e0bda9
k3v-5 <142853881+k3v-5@users.noreply.github.com>
2024-08-20 12:51:25 -0600

    actualized projects

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,99 +1,135 @@
 "use client";
-import React, { useTransition, useState } from 'react'
-import Image from 'next/image'
-import TabButton from './TabButton';
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
 
 const TAB_DATA = [
-    {
-        title:"Experience",
-        id:"experience",
-        content:(
-            <ul className='list-disc pl-2'>
-                <li>Lion System Solutions</li>
-                <p className='px-4'>I develop activities such as API connections, creation of components and views, and database management for an e-commerce platform using .NET, VUE, and SQL.</p>
-           
-                <li>Cuauhtemoc University</li>
-                <p className='px-4'>I carried out activities ranging from credentialing, access control, report generation, and support for administrative staff and teachers, to connecting cameras and security devices, and cabling.</p>
-                 </ul>
+  {
+    title: "Experience",
+    id: "experience",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Lion System Solutions</li>
+        <p className="px-4">
+          I develop activities such as API connections, creation of components
+          and views, and database management for an e-commerce platform using
+          .NET, VUE, and SQL.
+        </p>
 
-        )
-    },
-    {
-        
-        title:"Skills",
-        id: "skills",
-        content:(
-            <ul className='list-disc pl-2'>
-                <li>JavaScript</li>
-                <li>Python</li>
-                <li>.NET</li>
-                <li>React</li>
-                <li>Angular</li>
-                <li>Vue</li>
-                <li>MongoDb</li>
-                <li>SQL</li>
-            </ul>
-        )
-    },
-    {
-        title:"Education",
-        id:"education",
-        content:(
-            <ul className='list-disc pl-2'>
-                <li>Autonum University of Aguascalientes</li>
-                <p className='px-4'>I developed projects in areas of web programming such as a dating application with .Net and Angular; mobile development, such as a CRUD system; report generation using Power BI, Tableau, and Orange; machine learning algorithms with Python, such as emotion and facial detection; and a sentiment detector for tweets using web scraping.
-                </p>
-
-            </ul>
-        )
-    },
-
-]
+        <li>Cuauhtemoc University</li>
+        <p className="px-4">
+          I carried out activities ranging from credentialing, access control,
+          report generation, and support for administrative staff and teachers,
+          to connecting cameras and security devices, and cabling.
+        </p>
+      </ul>
+    ),
+  },
+  {
+    title: "Languajes",
+    id: "Languajes",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>.NET</li>
+        <li>SQL</li>
+        <li>NOSQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>React</li>
+        <li>Next</li>
+        <li>Vue</li>
+        <li>Angular</li>
+        <li>Tensorflow</li>
+        <li>Pytorch</li>
+        <li>Django</li>
+        <li>Node</li>
+        <li>Power Bi</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Autonomum University of Aguascalientes</li>
+        <p className="px-4">
+          I developed projects in areas of web programming such as a dating
+          application with .Net and Angular; mobile development, such as a CRUD
+          system; report generation using Power BI, Tableau, and Orange; machine
+          learning algorithms with Python, such as emotion and facial detection;
+          and a sentiment detector for tweets using web scraping.
+        </p>
+      </ul>
+    ),
+  },
+];
 
 const AboutSection = () => {
-    const [tab, setTab] = useState("skills");
-    const [isPending, startTransition] = useTransition();
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
 
-    const handleTabChange = (id) => {
-        startTransition(() => {
-            setTab(id);
-        });
-    }
-    return (
-        <section className='text-white'>
-            <div className='md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-                <Image src="/images/about-image.png" width={500} height={500} />
-                <div className='mt-4 md:mt-0 text-left flex flex-col h-full'>
-                    <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
-                    <p className='text-base lg:text-lg'>
-                    I am from Aguascalientes, Mexico, 23 years old, and have 2 years of experience in the technology field. I am very enthusiastic about learning, especially self-directed learning.                    </p>
-                    <div className='flex flex-row justify-start mt-8'>
-                    <TabButton
-                            selecTab={() => handleTabChange("experience")}
-                            active={tab === "experience"}>
-                            {" "}
-                            Experience{" "}
-                        </TabButton>
-                        <TabButton
-                            selecTab={() => handleTabChange("education")}
-                            active={tab === "education"}>
-                            {" "}
-                            Education{" "}
-                        </TabButton>
-                        <TabButton
-                            selecTab={() => handleTabChange("skills")}
-                            active={tab === "skills"}>
-                            {" "}
-                            Skills{" "}
-                        </TabButton>
-                        
-                        
-                    </div>
-                    <div className='mt-8'>{TAB_DATA.find((t)=> t.id===tab).content}</div>
-                </div>
-            </div>
-        </section>
-    )
-}
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.jpg" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am from Aguascalientes, Mexico, 23 years old, and have 2 years of
+            experience in the technology field. I am very enthusiastic about
+            learning, especially self-directed learning.{" "}
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selecTab={() => handleTabChange("experience")}
+              active={tab === "experience"}
+            >
+              {" "}
+              Experience{" "}
+            </TabButton>
+            <TabButton
+              selecTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selecTab={() => handleTabChange("Languajes")}
+              active={tab === "Languajes"}
+            >
+              {" "}
+              Programing Languajes{" "}
+            </TabButton>
+            <TabButton
+              selecTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AboutSection
+export default AboutSection;
--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -1,104 +1,117 @@
-"use client"
-import React, { useState } from 'react'
-import ProjectCard from './ProjectCard'
-import ProjectTag from './ProjectTag'
+"use client";
+import React, { useState } from "react";
+import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
 
 const ProjectsData = [
-    {
-        id: 1,
-        title: "Dating Aplication",
-        description: "Developed a dating application using Angular, .NET and sql. The application allows users to create an account, upload photos, and search for other users based on their preferences. The application also includes a chat feature that allows users to communicate with each other in real-time.",
-        image: "/images/projects/1.png",
-        tag: ["All", "Web"],
-        gitUrl: "/",
-        previewUrl: "/",
-    },
-    {
-        id: 2,
-        title: "Ecommerce Website",
-        description: "Developed an e-commerce website using Nextjs and PostgreSQL. The website allows users to browse products, add them to their cart, and checkout using a credit card. The website also includes an admin panel that allows the site owner to add, edit, and delete products.",
-        image: "/images/projects/2.jpg",
-        tag: ["All", "Web"],
-        gitUrl: "/",
-        previewUrl: "/",
-    },
-    {
-        id: 3,
-        title: "Crypto Tracker",
-        description: "Developed a cryptocurrency tracker using React and the CoinGecko API. The tracker allows users to search for cryptocurrencies and view their current price, market cap, and trading volume. The tracker also includes a watchlist feature that allows users to save their favorite cryptocurrencies.",
-        image: "/images/projects/3.jpg",
-        tag: ["All", "Web"],
-        gitUrl: "/",
-        previewUrl: "/",
-    },
-    {
-        id: 4,
-        title: "Data Science Project",
-        description: "Developed a cryptocurrency tracker using React and the CoinGecko API. The tracker allows users to search for cryptocurrencies and view their current price, market cap, and trading volume. The tracker also includes a watchlist feature that allows users to save their favorite cryptocurrencies.",
-        image: "/images/projects/3.jpg",
-        tag: ["All", "Data Science"],
-        gitUrl: "/",
-        previewUrl: "/",
-    },
-    {
-        id: 5,
-        title: "Data Science Project",
-        description: "Developed a cryptocurrency tracker using React and the CoinGecko API. The tracker allows users to search for cryptocurrencies and view their current price, market cap, and trading volume. The tracker also includes a watchlist feature that allows users to save their favorite cryptocurrencies.",
-        image: "/images/projects/3.jpg",
-        tag: ["All", "Data Science"],
-        gitUrl: "/",
-        previewUrl: "/",
-    }
-
-]
+  {
+    id: 1,
+    title: "Dating Aplication",
+    description:
+      "Developed a dating application using Angular, .NET and sql. The application allows users to create an account, upload photos, and search for other users based on their preferences. The application also includes a chat feature that allows users to communicate with each other in real-time.",
+    image: "/images/projects/dating-app/principal.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/k3v-5/CitasApp",
+    previewUrl: "https://github.com/k3v-5/CitasApp",
+  },
+  {
+    id: 2,
+    title: "Ecommerce Website",
+    description:
+      "Developed an e-commerce website using Nextjs and PostgreSQL. The website allows users to browse products, add them to their cart, and checkout using a credit card. The website also includes an admin panel that allows the site owner to add, edit, and delete products.",
+    image: "/images/projects/tesla-shop/principal.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 3,
+    title: "Crypto Tracker",
+    description:
+      "Developed a cryptocurrency tracker using React and the CoinGecko API. The tracker allows users to search for cryptocurrencies and view their current price, market cap, and trading volume. The tracker also includes a watchlist feature that allows users to save their favorite cryptocurrencies.",
+    image: "/images/projects/crypto-tracker/principal.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/k3v-5/CryptoTracker",
+    previewUrl: "/",
+  },
+  {
+    id: 4,
+    title: "Covid-19 Data Analysis",
+    description:
+      "Analyzed Covid-19 data using Python and the Pandas library. The analysis included calculating the total number of cases, deaths, and recoveries for each country. The analysis also included creating visualizations such as bar charts and line graphs to help visualize the data.",
+    image: "/images/projects/3.jpg",
+    tag: ["All", "Data Science"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 5,
+    title: "Emotion Detection",
+    description:
+      "Developed an emotion detection model using Python and the OpenCV library. The model uses a webcam to capture a user's face and then uses a pre-trained deep learning model to detect the user's emotion. The model then displays the user's emotion on the screen.",
+    image: "/images/projects/3.jpg",
+    tag: ["All", "Data Science"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Sentiment Analysis",
+    description:
+      "Developed a sentiment analysis model using Python . The model analyzes text data such as tweets and news articles to determine whether the sentiment is positive, negative, or neutral.",
+    image: "/images/projects/sentiment-analysis/principal.jpg",
+    tag: ["All", "Data Science"],
+    gitUrl: "https://github.com/k3v-5/Sentiment-analysis",
+    previewUrl: "https://github.com/k3v-5/Sentiment-analysis",
+  },
+];
 
 const ProjectsSection = () => {
-    const [tag, setTag] = useState("All");
-    const handleTagChange = (newTag) => {
-        setTag(newTag);
-    }
+  const [tag, setTag] = useState("All");
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
 
-    const filteredProjects = ProjectsData.filter((project)=>
-        project.tag.includes(tag)
-        );
+  const filteredProjects = ProjectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
 
-    return (
-        <>
-            <h2 className='text-center text-4xl font-bold text-white mt-4 mb-4'>
-                My projects
-            </h2>
-            <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
-                <ProjectTag
-                    onClick={handleTagChange}
-                    name="All"
-                    isSelected={tag === "All"}
-                />
-                <ProjectTag
-                    onClick={handleTagChange}
-                    name="Web"
-                    isSelected={tag === "Web"}
-                />
-                <ProjectTag
-                    onClick={handleTagChange}
-                    name="Data Science"
-                    isSelected={tag === "Data Science"}
-                />
-                
-            </div>
-            <div className='grid md:grid-cols-3 gap-8 md:gap-12'>
-                {filteredProjects.map((project) => (
-                    <ProjectCard
-                        key={project.id}
-                        title={project.title}
-                        description={project.description}
-                        imgUrl={project.image}
-                        gitUrl={project.gitUrl}
-                        previewUrl={project.previewUrl}
-                    />
-                ))}
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-4">
+        My projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Data Science"
+          isSelected={tag === "Data Science"}
+        />
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default ProjectsSection
+export default ProjectsSection;
