d6f3a967156864b2f40d33e0eebe62a7456da6ac
k3v-5 <kevingarrido711@gmail.com>
2026-03-24 21:16:54 -0600

    Ajustes color

--- src/app/components/Herosection.jsx
+++ src/app/components/Herosection.jsx
@@ -20,7 +20,7 @@ const Herosection = () => {
             {/* <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:bg-slate-200 text-white">
               Hire Me
             </button> */}
-            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-pink-500 via-purple-500 to-teal-500 hover:bg-slate-800 text-white border mt-3 ">
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-purple-400 to-white hover:bg-slate-800 text-white border mt-3 ">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
@@ -29,13 +29,13 @@ const Herosection = () => {
         </div>
         <div className="col-span-5 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
+            {/* <Image
               src="/images/portfolioprof.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
-            ></Image>
+            ></Image> */}
           </div>
         </div>
       </div>
--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,18 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const TabButton = ({ active, selecTab, children }) => {
-const buttonClasses = active 
-? "text-white border-b border-teal-500"
-: "text-[#ADB7BE]";
-    return (
-        <button onClick={selecTab}>
-            <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
-                {children}
-            </p>
-        </button>
-    );
+  const buttonClasses = active
+    ? "text-white border-b border-purple-400"
+    : "text-[#ADB7BE]";
+  return (
+    <button onClick={selecTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+        {children}
+      </p>
+    </button>
+  );
 };
 
-
-
-export default TabButton
+export default TabButton;
